e56d6d8f5b81809797d0f2641dff0b950c502803
Yuto-kimura <s1250172a@gmail.com>
2019-07-11 12:49:05 +0900

    test

--- datamanagement.py
+++ datamanagement.py
@@ -1,9 +1,23 @@
-class MenuItem2:
+import random
+
+class MenuItem:
+    def road(self):
+        print("Rolling the dice...")
+    def dais(self,daisA,daisB):
+        print("Die:"+str(daisA))
+        print("Die:"+str(daisB))
     def hello(self):
         print("Hello,"+self.name+"!")
 
 
-menu_item2 = MenuItem2()
+menu_item1 = MenuItem()
 print("What is your name?")
-menu_item2.name = input(">")
-menu_item2.hello()
+menu_item1.name = input(">")
+menu_item1.hello()
+menu_item1.road()
+daisA = random.randint(1,6)
+daisB = random.randint(1,6)
+
+result = daisA + daisB
+menu_item1.dais(daisA,daisB)
+print("Total value:"+str(result))
