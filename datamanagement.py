e48446728a8ced1f96300fb17e02cf0747786afe
Yuto-kimura <s1250172a@gmail.com>
2019-07-11 10:53:38 +0900

    Greetings Added

--- datamanagement.py
+++ datamanagement.py
@@ -1,19 +1,9 @@
-import random
-
-class MenuItem:
-    def road(self):
-        print("Rolling the dice...")
-    def dais(self,daisA,daisB):
-        print("Die:"+str(daisA))
-        print("Die:"+str(daisB))
+class MenuItem2:
+    def hello(self):
+        print("Hello,"+self.name+"!")
 
 
-
-menu_item1 = MenuItem()
-menu_item1.road()
-daisA = random.randint(1,6)
-daisB = random.randint(1,6)
-
-result = daisA + daisB
-menu_item1.dais(daisA,daisB)
-print("Total value:"+str(result))
+menu_item2 = MenuItem2()
+print("What is your name?")
+menu_item2.name = input(">")
+menu_item2.hello()
